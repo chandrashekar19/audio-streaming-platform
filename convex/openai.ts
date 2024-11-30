694cf2a0bb4b4d110df8297a14522c6f824f3b4b
KalalChandu <kalalshannu19@gmail.com>
2024-11-30 22:05:27 +0530

    resloved: the api key issue

--- convex/openai.ts
+++ convex/openai.ts
@@ -4,11 +4,8 @@ import { v } from "convex/values";
 import OpenAI from "openai";
 import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 
-const OPENAI_API_KEY =
-  "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";
-
 const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const generateAudioAction = action({
